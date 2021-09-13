ca4aef28745a273a0f82ad54f6f9b0f5936a57ee
samarasilva18 <samarasilvacandy@gmail.com>
2021-09-13 13:52:37 -0300

    Dessa vez terminei mesmo

--- src/components/Cadastro/Cadastro.jsx
+++ src/components/Cadastro/Cadastro.jsx
@@ -8,12 +8,12 @@ export default function Cadastro(props) {
          <CadastroHeader/>
         <div id="container">
             <div class="cadasbody">
-            <h2>Cadastre-se para receber atualizações e noticias sobre descobertas do Egito Antigo!</h2>
+            <h2>Cadastre-se para receber notícias sobre descobertas do Egito Antigo!</h2>
             <form>
                 <fieldset>
                     <legend>Seus dados:</legend>
                     <div class="input-block">
-                        <label for="name">Nome completo:</label>
+                        <label for="name">Nome Completo:</label>
                         <input name="name" id="name" required/>
                     </div>
 
@@ -22,18 +22,20 @@ export default function Cadastro(props) {
                         <input name="email" id="email" required/>
                     </div>
 
-                    <div class="input-blocktel">
+                    <div class="input-block">
                         <label for="whatsapp">Telefone: </label>
                         <input name="whatsapp" id="whatsapp" type="number" required/>
                     </div>
-                    <div className="insira">Insira também seu endereço e CEP para receber nossa revista!</div>
+                    <div className="insira">Insira também seus dados postais para receber nossa revista!</div>
                     <div class="input-block">
                         <label for="address">Endereço: <small>*(Opcional):</small></label>
                         <input name="address" id="address"/>
                     </div>
                     <div class="input-blocktel">
+                        <label for="rg">RG: <small>*(Opcional):</small></label>
+                        <input name="rg" id="rg" type="number" className="input1"/>
                         <label for="cep">CEP: <small>*(Opcional):</small></label>
-                        <input name="cep" id="cep" type="number"/>
+                        <input name="cep" id="cep" type="number" className="input2"/>
                     </div>
                     <button type="submit">Salvar Cadastro</button>
                 </fieldset>
